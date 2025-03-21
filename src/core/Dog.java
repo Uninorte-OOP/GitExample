d4ebe9067336d3eb63af2fb9483fb92d228aeb58
Eduardo David Angulo Madrid <edangulo@uninorte.edu.co>
2025-03-21 15:51:52 -0500

    Feat: Added Dog class

---
+++ src/core/Dog.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author edangulo
+ */
+public class Dog {
+    
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    private String race;
+
+    public Dog(String name, int age, double height, double weight, String race) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.race = race;
+    }
+    
+}
